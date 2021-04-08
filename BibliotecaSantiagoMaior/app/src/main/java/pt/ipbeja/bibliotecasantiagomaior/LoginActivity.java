12454f8e30acfb76a3d5b2614a2081170f4421d0
Gnomo000 <21707@stu.ipbeja.pt>
2021-04-08 17:02:07 +0100

    Alteração do Projecto

---
+++ BibliotecaSantiagoMaior/app/src/main/java/pt/ipbeja/bibliotecasantiagomaior/LoginActivity.java
@@ -0,0 +1,14 @@
+package pt.ipbeja.bibliotecasantiagomaior;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
