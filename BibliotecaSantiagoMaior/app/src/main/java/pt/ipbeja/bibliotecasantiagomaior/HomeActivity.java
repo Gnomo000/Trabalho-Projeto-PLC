d3bb318c6976430921fbbe40daa9e7dee3acab19
Vazinho <21132@stu.ipbeja.pt>
2021-04-14 12:25:12 +0100

    atualizar projeto 14/04

---
+++ BibliotecaSantiagoMaior/app/src/main/java/pt/ipbeja/bibliotecasantiagomaior/HomeActivity.java
@@ -0,0 +1,14 @@
+package pt.ipbeja.bibliotecasantiagomaior;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
