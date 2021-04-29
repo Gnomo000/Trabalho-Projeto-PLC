e6d45a2195e7ee1d10657861d598b7920a9fcaa8
Gnomo000 <21707@stu.ipbeja.pt>
2021-04-29 16:50:14 +0100

    Alteração do Projecto

--- BibliotecaSantiagoMaior/app/src/main/java/pt/ipbeja/bibliotecasantiagomaior/SettingsActivity.java
+++ BibliotecaSantiagoMaior/app/src/main/java/pt/ipbeja/bibliotecasantiagomaior/SettingsActivity.java
@@ -1,14 +1,40 @@
 package pt.ipbeja.bibliotecasantiagomaior;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Toast;
+import com.polyak.iconswitch.IconSwitch;
 import android.os.Bundle;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private IconSwitch iconSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        // initializing the IconSwitch object
+        iconSwitch = (IconSwitch) findViewById(R.id.iconSwitch);
+
+        // IconSwitch  Checked Change Listener
+        iconSwitch.setCheckedChangeListener(new IconSwitch.CheckedChangeListener() {
+        @Override
+        public void onCheckChanged(IconSwitch.Checked current) {
+            //simple witch case
+            switch (current) {
+
+                case LEFT:
+                    //showing simple toast message to the user
+                    Toast.makeText(SettingsActivity.this, "Light Mode", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case RIGHT:
+                    //showing simple toast message to the user
+                    Toast.makeText(SettingsActivity.this, "Dark Mode", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+        });
     }
 }
