50ad46078de4aa11bf698e0ed6fd77f794c1a8b1
Gnomo000 <21707@stu.ipbeja.pt>
2021-05-10 17:16:08 +0100

    Alteração do Projeco
    10/05/2021

--- BibliotecaSantiagoMaior/app/src/main/java/pt/ipbeja/bibliotecasantiagomaior/SettingsActivity.java
+++ BibliotecaSantiagoMaior/app/src/main/java/pt/ipbeja/bibliotecasantiagomaior/SettingsActivity.java
@@ -1,9 +1,13 @@
 package pt.ipbeja.bibliotecasantiagomaior;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 import com.polyak.iconswitch.IconSwitch;
 import android.os.Bundle;
@@ -20,16 +24,28 @@ public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-
         this.sharedPref = SettingsActivity.this.getSharedPreferences(
                 getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+
+        String selectedTheme = this.sharedPref.getString(SETTINGS_THEME, THEME_LIGHT);
+        switch (selectedTheme){
+            case THEME_DARK:
+                setTheme(R.style.Theme_BibliotecaSantiagoMaior_Dark_Default);
+                Log.i("Teste","Escuro");
+                break;
+            case THEME_LIGHT:
+                setTheme(R.style.Theme_BibliotecaSantiagoMaior_Light_Default);
+                Log.i("Teste","Claro");
+                break;
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
 
         // initializing the IconSwitch object
         iconSwitch = (IconSwitch) findViewById(R.id.iconSwitch);
 
-        String selectedTheme = this.sharedPref.getString(SETTINGS_THEME, THEME_LIGHT);
+
         switch (selectedTheme) {
             case THEME_DARK:
                 iconSwitch.setChecked(IconSwitch.Checked.RIGHT);
@@ -53,20 +69,26 @@ public class SettingsActivity extends AppCompatActivity {
                 case LEFT:
                     //showing simple toast message to the user
                     Toast.makeText(SettingsActivity.this, "Tema Claro", Toast.LENGTH_SHORT).show();
-                    System.out.println("Olá");
                     editor.putString(SETTINGS_THEME, THEME_LIGHT);
                     editor.apply();
+                    recreate();
                     break;
 
                 case RIGHT:
-                    //showing simple toast message to the user
                     Toast.makeText(SettingsActivity.this, "Tema Escuro", Toast.LENGTH_SHORT).show();
-                    System.out.println("Adeus");
                     editor.putString(SETTINGS_THEME, THEME_DARK);
                     editor.apply();
+                    recreate();
                     break;
             }
         }
         });
+    }
+
+    public void onClickAboutUs(View view) {
+        AlertDialog.Builder alertAboutUs = new AlertDialog.Builder(this);
+        alertAboutUs.setTitle("Sobre Nós");
+        alertAboutUs.setMessage("ETC");
+        alertAboutUs.create().show();
     }
 }
