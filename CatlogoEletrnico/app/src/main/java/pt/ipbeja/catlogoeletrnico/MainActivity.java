aa4c9bb0025a8fed665c7f65977505a7af612c18
Gnomo000 <21707@stu.ipbeja.pt>
2021-06-03 22:29:08 +0100

    Criação do Novo projeto (Mesmo tema mas cmeçar de novo) (2)

--- CatlogoEletrnico/app/src/main/java/pt/ipbeja/catlogoeletrnico/MainActivity.java
+++ CatlogoEletrnico/app/src/main/java/pt/ipbeja/catlogoeletrnico/MainActivity.java
@@ -1,8 +1,15 @@
 package pt.ipbeja.catlogoeletrnico;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+
+    public void goRegister(View view) {
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void goLogin(View view) {
     }
 }
