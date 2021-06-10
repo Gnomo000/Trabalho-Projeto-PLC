b93c0b6c8f951de16b65407a8e8774db3a3b456f
Vazinho <21132@stu.ipbeja.pt>
2021-06-10 18:01:28 +0100

    10/6 num 3

--- CatlogoEletrnico/app/src/main/java/pt/ipbeja/catlogoeletrnico/BooksActivity.java
+++ CatlogoEletrnico/app/src/main/java/pt/ipbeja/catlogoeletrnico/BooksActivity.java
@@ -1,14 +1,45 @@
 package pt.ipbeja.catlogoeletrnico;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class BooksActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books);
+        HomeActivity.isActive = true;
+
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(HomeActivity.isActive = true){
+            Button button = findViewById(R.id.buttonBooks);
+            button.setClickable(false);
+            button.setBackgroundColor(0xFFFFFF);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
--- CatlogoEletrnico/app/src/main/java/pt/ipbeja/catlogoeletrnico/HomeActivity.java
+++ CatlogoEletrnico/app/src/main/java/pt/ipbeja/catlogoeletrnico/HomeActivity.java
@@ -8,17 +8,20 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
-
+    public static boolean isActive = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        isActive = true;
 
         Toolbar toolbar = findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
@@ -28,6 +31,12 @@ public class HomeActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        if(isActive = true){
+            Button button = findViewById(R.id.buttonMain);
+            button.setClickable(false);
+            button.setBackgroundColor(0xFFFFFF);
+        }
     }
 
     @Override
@@ -42,10 +51,12 @@ public class HomeActivity extends AppCompatActivity {
     public void goToHistory(View view) {
         Intent intent = new Intent(this,HistoryActivity.class);
         startActivity(intent);
+        isActive = false;
     }
 
-    /*public void goToBooks(View view) {
+    public void goToBooks(View view) {
         Intent intent = new Intent(this,BooksActivity.class);
         startActivity(intent);
-    }*/
+        isActive = false;
+    }
 }
