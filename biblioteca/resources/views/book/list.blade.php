fb919b32638e8ca08a0ac03f17ae857e3af47d21
Gnomo000 <danixandrerodrigues@gmail.com>
2022-02-01 01:33:50 +0000

    blow

--- biblioteca/resources/views/book/list.blade.php
+++ biblioteca/resources/views/book/list.blade.php
@@ -1,7 +1,6 @@
 @extends('book.layouts.app')
 
 @section('content')
-<<<<<<< Updated upstream
     <div class="card-header">{{ __('Dashboard') }}</div>
     <div class="col-lg-1">
     </div>
@@ -60,59 +59,4 @@
 
         {{ __('You are logged in!') }}
     </div>
-=======
-<div class="card-header">{{ __('Dashboard') }}</div>
-<div class="col-lg-1"></div>
-<div class="card-body">
-    @if (session('status'))
-        <div class="alert alert-success" role="alert">
-            {{ session('status') }}
-        </div>
-    @endif
-    <table class="table table-bordered">
-        <tr>
-            <th>No</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Address</th>
-            <th>Address</th>
-            <th>Address</th>
-            <th>Address</th>
-            <th>Address</th>
-            <th>Address</th>
-            <th width="280px">Action</th>
-        </tr>
-        @php
-            $i = 0;
-        @endphp
-        @foreach ($books as $book)
-            <tr>
-                <td>{{ ++$i }}</td>
-                <td>{{ $book->title }}</td>
-                <td>{{ $book->titleEn }}</td>
-                <td>{{ $book->author }}</td>
-                <td>{{ $book->edition }}</td>
-                <td>{{ $book->publisher }}</td>
-                <td>{{ $book->synopse }}</td>
-                <td>{{ $book->genders }}</td>
-                <td>{{ $book->quantity }}</td>
-                <td>{{ $book->image }}</td>
-                <td>
-                    <form action="{{ route('book.destroy',$book->id) }}" method="POST">
-                        <a class="btn btn-info" href="{{ route('book.show',$book->id) }}">Show</a>
-                        <a class="btn btn-primary" href="{{ route('book.edit',$book->id) }}">Edit</a>
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Delete</button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-    </table>
-
-    
-
-    {{ __('You are logged in!') }}
-</div>
->>>>>>> Stashed changes
 @endsection
