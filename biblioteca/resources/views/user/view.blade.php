99bd15054ae56859e495097fcd56b47a4e846122
Vazinho <21132@stu.ipbeja.pt>
2022-02-01 03:41:10 +0000

    THE END IS HERE!

--- biblioteca/resources/views/user/view.blade.php
+++ biblioteca/resources/views/user/view.blade.php
@@ -1,6 +1,8 @@
 @extends('user.layouts.app')
 
 @section('content')
+@if (Route::has('login'))
+@auth
 <div class="card-header">{{ __('Ver User') }}</div>
 <div class="col-lg-1"></div>
 <div class="card-body">
@@ -44,4 +46,16 @@
     </table>
     {{ __('You are logged in!') }}
 </div>
+
+<div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+    @else
+        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+        @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+        @endif
+    @endauth
+</div>
+    @endif
 @endsection
